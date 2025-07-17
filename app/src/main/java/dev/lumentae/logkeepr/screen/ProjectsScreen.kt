fa583fd6c7aa4748673c5c74f08223670e36b80d
Fynn Gutic <66220444+Fynn93@users.noreply.github.com>
2025-07-17 16:52:46 +0200

    Initial commit

---
+++ app/src/main/java/dev/lumentae/logkeepr/screen/ProjectsScreen.kt
@@ -0,0 +1,10 @@
+package dev.lumentae.logkeepr.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProjectsScreen(modifier: Modifier) {
+    Text("Projects Screen", modifier = modifier)
+}
---
+++ app/src/main/java/dev/lumentae/logkeepr/screen/StatsScreen.kt
@@ -0,0 +1,10 @@
+package dev.lumentae.logkeepr.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun StatsScreen(modifier: Modifier) {
+    Text("Stats Screen", modifier = modifier)
+}
