753867e66a9388d2b0a255f14b288ca924df435f
lumentae <66220444+lumentae@users.noreply.github.com>
2025-07-23 21:28:39 +0200

    feat: Implement DefaultPageTemplate for consistent screen layout in Home, Projects, Stats, and Settings screens

---
+++ app/src/main/java/dev/lumentae/logkeepr/screen/settings/SettingsScreen.kt
@@ -0,0 +1,14 @@
+package dev.lumentae.logkeepr.screen.settings
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dev.lumentae.logkeepr.screen.components.DefaultPageTemplate
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsScreen(modifier: Modifier) {
+    DefaultPageTemplate("Settings", modifier) {
+
+    }
+}
