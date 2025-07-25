1c573631912a4cd2053c671d67cd92ad90941e68
lumentae <66220444+lumentae@users.noreply.github.com>
2025-07-26 00:15:47 +0200

    feat: Add language preference with dropdown selection in settings

--- app/src/main/java/dev/lumentae/logkeepr/data/preferences/Preferences.kt
+++ app/src/main/java/dev/lumentae/logkeepr/data/preferences/Preferences.kt
@@ -8,7 +8,8 @@ import dev.lumentae.logkeepr.R
 class PreferenceKey<T>(
     var key: Preferences.Key<T>,
     var value: MutableState<T>,
-    var translationKey: Int
+    var translationKey: Int,
+    var extras: Any = Unit,
 )
 
 class PreferenceSection(
@@ -18,12 +19,20 @@ class PreferenceSection(
 
 object Preferences {
     object Sections {
-        val theme = PreferenceSection(
-            R.string.config_section_theme
+        val general = PreferenceSection(
+            R.string.config_section_general
         )
     }
 
     object Keys {
+        val language = PreferenceKey(
+            stringPreferencesKey("language"), mutableStateOf("en"),
+            R.string.config_entry_language,
+            mapOf(
+                "en" to R.string.language_english,
+                "de" to R.string.language_german,
+            )
+        )
         val useDarkMode = PreferenceKey(
             booleanPreferencesKey("useDarkMode"), mutableStateOf(false),
             R.string.config_entry_useDarkMode
